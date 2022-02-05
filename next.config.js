1969bd5ed707d9a512f142e158da1d27eb5200ff
Scott <26013935+Pieparker@users.noreply.github.com>
2022-02-05 23:17:10 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ module.exports = {
         destination: '/usa',
         permanent: false,
       },
+            {
+        source: '/test',
+        has: [
+          {
+            type: 'header',
+            key: 'x-vercel-ip-country',
+            value: 'GB',
+          },
+        ],
+        destination: '/gb',
+        permanent: false,
+      },
     ];
   },
 };
